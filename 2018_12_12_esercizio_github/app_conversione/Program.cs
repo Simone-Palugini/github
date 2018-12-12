7f9535d351bbe92b37befec924e774450f05d822
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-12 08:36:30 +0100

    primo codice per convertire un numero in binario

---
+++ 2018_12_12_esercizio_github/app_conversione/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_conversione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string risultato = ""; int num = 255;
+
+            while (num > 1)
+            {
+                int resto = num % 2;
+                risultato = Convert.ToString(resto) + risultato;
+                num = num / 2;
+            }
+            risultato = Convert.ToString(num) + risultato;
+            Console.WriteLine($"il numero binario è {risultato}");
+        }
+    }
+}
